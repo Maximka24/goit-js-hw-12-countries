c67fb196eb5369b62704a024915804ef119efcdd
Kovalenko Makcim <max.kow@yandex.ru>
2021-06-19 00:24:02 +0300

    дз 12
    Выполнено ДЗ без разметки ошибки

--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
-import './sass/main.scss';
+import API from './js/fetchCountries';
+
+import itemCardCount from './partials/count-card.hbs';
+import itemCardCountList from './partials/count-card-list.hbs';
+
+const debounce = require('lodash.debounce');
+
+const refs = {
+	errorParagraph: document.querySelector('.js-render-error'),
+	countriesList: document.querySelector('.js-render-countries'),
+	inputDate: document.querySelector('.js-get-input')
+};
+
+refs.inputDate.addEventListener('input', debounce(onGetBecCountri, 500));
+
+function onGetBecCountri() {
+	const value = refs.inputDate.value;
+
+	API.fetchCountries(value)
+		.then(renderCountriCard)
+		.catch(error => console.log(error))
+		// .finally()
+}
+
+function renderCountriCard(countries){
+	if (countries.length < 10 && countries.length >=2) {
+		const markup = itemCardCountList(countries);
+		refs.countriesList.innerHTML = markup;		
+	}
+
+	if (countries.length === 1) {
+		const markup = itemCardCount(countries);			
+		refs.countriesList.innerHTML = markup;
+	}
+}
+
